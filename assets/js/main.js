ff8b2b2a1c71f6aa9b60fdc5cba6759a4b820ff4
Fernanda Vergara <fernandavmelin@gmail.com>
2020-02-19 17:00:13 -0300

    ciclos y metodos en js

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+/*OPCIÓN 1 */
+
+// var numero = parseInt (prompt('Por favor, Ingrese un número del 1 al 100', 0));
+/*for(i = 1 ; i <= numero ; i++){
+        console.log(i);
+    }*/
+
+
+/*OPCIÓN 2*/
+
+// do {
+//     var pregunta= prompt('¿De qué color es el caballo blanco de Napoleón?');
+// }while(pregunta != 'blanco');
+
+/*OPCIÓN 3*/
+// promedio =[] 
+//     promedio.push(parseInt(prompt('Por favor, ingrese su promedio de matemáticas', 0)))  
+//     promedio.push(parseInt(prompt('Por favor, ingrese su promedio de física', 0)))  
+//     promedio.push(parseInt(prompt('Por favor, ingrese su promedio de ciencias', 0)))  
+
+// alert('Tu promedio general es: ')
+
+/*OPCIÓN 4 */
+var frutas = prompt('Ingresa tres frutas')
+var frutas_array = frutas.split(' ');
+
+console.log(frutas_array);
